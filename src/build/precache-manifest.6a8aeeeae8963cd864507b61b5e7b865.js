15c02b1226d441a73856ea500831d77cfd4fced5
Nick Lionakis <nick.lionakis@gmail.com>
2020-10-05 12:32:17 +0300

    First commit

---
+++ src/build/precache-manifest.6a8aeeeae8963cd864507b61b5e7b865.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a626fbd5686667072fb30cafeb912f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "51269222da1955a531c1",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "f7742bd7ca531cfad47f",
+    "url": "/static/js/2.b09d3ca3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b09d3ca3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51269222da1955a531c1",
+    "url": "/static/js/main.08b7ad47.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
